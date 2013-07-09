38cb252429b12595973ca78fa543b857cd3f303c
Helder Couto <helder.couto@icloud.com>
2013-07-09 19:09:51 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var indexbuf = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexbuf.toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
